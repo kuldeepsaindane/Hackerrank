6148552255a514ceafe3232448eac89b5278037f
kuldeepsaindane <76591048+kuldeepsaindane@users.noreply.github.com>
2021-08-12 11:08:20 +0530

    Update JavaStdinAndStdoutI.java

--- Java/Introduction/JavaStdinAndStdoutI.java
+++ Java/Introduction/JavaStdinAndStdoutI.java
@@ -30,7 +30,7 @@ Sample Output
 
 import java.util.*;
 
-public class Solution {
+public class JavaStdinAndStdoutI {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
